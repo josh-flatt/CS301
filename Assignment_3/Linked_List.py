9cecfd8a648a229eafa49a2e3c8e4d0a9417b2da
Josh Flatt <josh.m.flatt@gmail.com>
2023-02-14 15:16:04 -0700

    further work

--- Assignment_3/Linked_List.py
+++ Assignment_3/Linked_List.py
@@ -17,40 +17,96 @@ class Linked_List():
         if(self.head_node == None): 
             self.head_node = new_Node
             return
+        after_node = self.head_node
+        self.head_node = new_Node
+        new_Node.set_next(after_node)
+            
+    def remove(self, item):
+        currNode = self.head_node
+        prevNode = self.head_node
+        if(self.head_node.get_value() == item):
+            self.head_node = self.head_node.get_next()
+            return
+        while(currNode != None):
+            if(currNode.get_value() == item):
+                prevNode.set_next(currNode.get_next())
+                return
+            prevNode = currNode
+            currNode = currNode.get_next()
+        raise Exception("Item not in list!")
+            
+    def search(self, item):
+        current_node = self.head_node
+        while(current_node.get_value() != item):
+            if(current_node.get_next() == None):
+                return False
+            current_node = current_node.get_next()
+        return True
+    
+    def isEmpty(self):
+        if(self.head_node == None):
+            return True
+        return False
+    
+    def size(self):
+        current_node = self.head_node
+        size = 0
+        while(current_node != None):
+            size += 1
+            current_node = current_node.get_next()
+        return size
+    
+    def append(self, item):
+        new_Node = Node(item)
+        if(self.head_node == None): 
+            self.head_node = new_Node
+            return
         currNode = self.head_node
         while (currNode.get_next() != None):
             currNode = currNode.get_next()
         currNode.set_next(new_Node)
-            
-    def remove(self, item):
-        currNode = self.head_node
-        prevNode = None
-        while(currNode.get_value() != item):
-            prevNode = currNode
-            currNode = currNode.get_next()
         
-        
-    def search(self, item):
-        current_node = self.head_node
-        while(current_node.get_value != item):
-            current_node = current_node.get_next()
-        return current_node
-    
-    def isEmpty(self):
-        pass
-    def size(self):
-        pass
-    def append(self, item):
-        pass
     def index(self, item):
-        pass
+        if(not self.search(item)):
+            raise Exception("Item not in list!")
+        index = 0
+        current_node = self.head_node
+        while(current_node.get_value() != item):
+            index += 1
+            current_node = current_node.get_next()
+        return index
+        
     def insert(self, position, item):
-        pass
-    def pop(self, position = "Last####"):
+        if(position > self.size()):
+            raise IndexError()
+        currNode = self.head_node
+        index = 0
+        prevNode = self.head_node
+        while(position > index):
+            pass
+    
+    def pop(self, position = size() - 1):
         pass
 
 if(__name__ == "__main__"):
     n = Linked_List()
     n.add(5)
+    print(n)
+    print(f"size: {n.size()}")
     n.add("bean")
     print(n)
+    print(f"size: {n.size()}")
+    n.remove("bean")
+    print(n)
+    print(f"size: {n.size()}")
+    n.add("new")
+    print(n)
+    print(f"size: {n.size()}")
+    n.remove(5)
+    print(n)
+    print(f"size: {n.size()}")
+    print(f"index of new: {n.index('new')}")
+    n.add("first")
+    print(n)
+    print(f"size: {n.size()}")
+    print(f"index of new: {n.index('new')}")
