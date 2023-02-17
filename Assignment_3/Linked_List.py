68f2ec4567eeed9ad2a99c44a4949819f6f68d5e
Josh Flatt <josh.m.flatt@gmail.com>
2023-02-16 20:18:20 -0700

    Completed Linked list implementation, added previous node field, getter, setter to Node class

--- Assignment_3/Linked_List.py
+++ Assignment_3/Linked_List.py
@@ -1,141 +1,151 @@
 from Node import Node
 
-class Linked_List():
+
+class Linked_List:
     def __init__(self):
-        self.head_node = None
-    
+        self.headNode = None
+
     def __str__(self):
         output = ""
-        current_node = self.head_node
-        while (current_node != None):
-            output += f"{current_node.get_value()}, "
-            current_node = current_node.get_next()
+        currNode = self.headNode
+        while currNode != None:
+            output += f"{currNode.get_value()}, "
+            currNode = currNode.get_next()
         return f"[{output[:-2]}]"
-    
+
     def add(self, item):
-        new_Node = Node(item)
-        if(self.head_node == None): 
-            self.head_node = new_Node
+        newNode = Node(item)
+        if self.headNode == None:
+            self.headNode = newNode
             return
-        after_node = self.head_node
-        self.head_node = new_Node
-        new_Node.set_next(after_node)
-            
+        nextNode = self.headNode
+        self.headNode = newNode
+        newNode.set_next(nextNode)
+
     def remove(self, item):
-        currNode = self.head_node
-        prevNode = self.head_node
-        if(self.head_node.get_value() == item):
-            self.head_node = self.head_node.get_next()
+        currNode = self.headNode
+        prevNode = self.headNode
+        if self.headNode.get_value() == item:
+            self.headNode = self.headNode.get_next()
             return
-        while(currNode != None):
-            if(currNode.get_value() == item):
+        while currNode != None:
+            if currNode.get_value() == item:
                 prevNode.set_next(currNode.get_next())
                 return
             prevNode = currNode
             currNode = currNode.get_next()
         raise Exception("Item not in list!")
-            
+
     def search(self, item):
-        current_node = self.head_node
-        while(current_node.get_value() != item):
-            if(current_node.get_next() == None):
+        currNode = self.headNode
+        while currNode.get_value() != item:
+            if currNode.get_next() == None:
                 return False
-            current_node = current_node.get_next()
+            currNode = currNode.get_next()
         return True
-    
+
     def isEmpty(self):
-        if(self.head_node == None):
+        if self.headNode == None:
             return True
         return False
-    
+
     def size(self):
-        current_node = self.head_node
+        currNode = self.headNode
         size = 0
-        while(current_node != None):
+        while currNode != None:
             size += 1
-            current_node = current_node.get_next()
-        return size
-    
-    def append(self, item):
-        new_Node = Node(item)
-        if(self.head_node == None): 
-            self.head_node = new_Node
-            return
-        currNode = self.head_node
-        while (currNode.get_next() != None):
             currNode = currNode.get_next()
-        currNode.set_next(new_Node)
-        
+        return size
+
+    def append(self, item):
+        new_node = Node(item)
+        if self.headNode == None:
+            self.headNode = new_node
+            return
+        currNode = self.headNode
+        while currNode.get_next() != None:
+            currNode = currNode.get_next()
+        currNode.set_next(new_node)
+
     def index(self, item):
-        if(not self.search(item)):
+        if not self.search(item):
             raise Exception("Item not in list!")
         index = 0
-        current_node = self.head_node
-        while(current_node.get_value() != item):
+        currNode = self.headNode
+        while currNode.get_value() != item:
             index += 1
-            current_node = current_node.get_next()
+            currNode = currNode.get_next()
         return index
-        
+
     def insert(self, position, item):
-        if(position > self.size() or position < 0):
+        if position > self.size() or position < 0:
             raise IndexError()
         newNode = Node(item)
-        currNode = self.head_node
+        currNode = self.headNode
+        prevNode = self.headNode
         index = 0
-        prevNode = self.head_node
-        while(position != index):
+        while position != index:
             prevNode = currNode
             currNode = currNode.get_next()
             index += 1
-        if(position != 0):
+        if position != 0:
             prevNode.set_next(newNode)
             newNode.set_next(currNode)
-        if(position == 0):
+        if position == 0:
             newNode.set_next(currNode)
-            self.head_node = newNode
-            
-    def pop(self, position = None):
-        if(position == None):
-            position = self.size() - 1
-        if(position > self.size() or position < 0):
-            raise IndexError()
-        currNode = self.head_node
-        index = 0
-        prevNode = self.head_node
-        while(position != index):
-            prevNode = currNode
-            currNode = currNode.get_next()
-            index += 1
-        if(position != 0):
-            prevNode.set_next(newNode)
-            newNode.set_next(currNode)
-        if(position == 0):
-            newNode.set_next(currNode)
-            self.head_node = newNode
-        
+            self.headNode = newNode
 
-if(__name__ == "__main__"):
-    n = Linked_List()
-    n.add(5)
-    print(n)
-    print(f"size: {n.size()}")
-    n.add("bean")
-    print(n)
-    print(f"size: {n.size()}")
-    n.remove("bean")
-    print(n)
-    print(f"size: {n.size()}")
-    n.add("new")
-    print(n)
-    print(f"size: {n.size()}")
-    n.remove(5)
-    print(n)
-    print(f"size: {n.size()}")
-    print(f"index of new: {n.index('new')}")
-    n.add("first")
-    print(n)
-    print(f"size: {n.size()}")
-    print(f"index of new: {n.index('new')}")
-    n.insert(0, "ins")
-    print(n)
-    print(f"size: {n.size()}")
+    def pop(self, position=None):
+        if position == None:
+            position = self.size() - 1
+        if position >= self.size() or position < 0:
+            raise IndexError()
+        currNode = self.headNode
+        prevNode = self.headNode
+        index = 0
+        while position != index:
+            prevNode = currNode
+            currNode = currNode.get_next()
+            index += 1
+        if position != 0:
+            prevNode.set_next(currNode.get_next())
+            return currNode.get_value()
+        if position == 0:
+            self.headNode = currNode.get_next()
+            return currNode.get_value()
+
+
+# if __name__ == "__main__":
+    
+    ########### Testing ###########
+    
+    # linked_list = Linked_List()
+    # linked_list.add(5)
+    # print(linked_list)
+    # print(f"size: {linked_list.size()}")
+    # linked_list.add("test")
+    # print(linked_list)
+    # print(f"size: {linked_list.size()}")
+    # linked_list.remove("test")
+    # print(linked_list)
+    # print(f"size: {linked_list.size()}")
+    # linked_list.add("new")
+    # print(linked_list)
+    # print(f"size: {linked_list.size()}")
+    # linked_list.remove(5)
+    # print(linked_list)
+    # print(f"size: {linked_list.size()}")
+    # print(f"index of new: {linked_list.index('new')}")
+    # linked_list.add("first")
+    # print(linked_list)
+    # print(f"size: {linked_list.size()}")
+    # print(f"index of new: {linked_list.index('new')}")
+    # linked_list.insert(0, "ins")
+    # print(linked_list)
+    # print(f"size: {linked_list.size()}")
+    # print(f'Popped "{linked_list.pop()}"')
+    # print(linked_list)
+    # print(f"size: {linked_list.size()}")
+    # print(f'Popped "{linked_list.pop(0)}"')
+    # print(linked_list)
+    # print(f"size: {linked_list.size()}")
--- Assignment_3/Node.py
+++ Assignment_3/Node.py
@@ -2,12 +2,19 @@ class Node():
     def __init__(self, item):
         self.item = item
         self.nextNode = None
+        self.prevNode = None
 
+    def get_value(self):
+        return self.item
+    
     def get_next(self):
         return self.nextNode
     
-    def set_next(self, new_node):
-        self.nextNode = new_node
+    def set_next(self, newNode):
+        self.nextNode = newNode
         
-    def get_value(self):
-        return self.item
+    def get_prev(self):
+        return self.prevNode
+    
+    def set_prev(self, newNode):
+        self.prevNode = newNode
