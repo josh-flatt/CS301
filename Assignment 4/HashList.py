85f058d4d839f4341b0e5cda6e4f348860b161d7
Josh Flatt <josh.m.flatt@gmail.com>
2023-03-09 15:06:27 -0700

    Update HashList.py

--- Assignment 4/HashList.py
+++ Assignment 4/HashList.py
@@ -10,6 +10,17 @@ class HashList:
     def hashFunction(self, item):
         return item % self.length
 
+    def rehash(self, index):
+        first = index
+        while True:
+            if self.HashList[index] == None:
+                return index
+            index += 1
+            if index >= len(self.HashList):
+                index = 0
+            if index == first:
+                raise IndexError
+
     def put(self, item):
         index = self.hashFunction(item)
         first = index
@@ -18,9 +29,37 @@ class HashList:
         while True:
             if self.HashList[index] == None:
                 self.HashList[index] = item
+                self.item_count += 1
                 return
             index += 1
-            if index > len(self.HashList):
+            if index >= len(self.HashList):
                 index = 0
             if index == first:
                 return False
+
+    def contains(self, item):
+        index = self.hashFunction(item)
+        first = index
+        if self.HashList[index] == item:
+            return True
+        if self.hashList[index] == None:
+            return False
+        while True:
+            index += 1
+            if index >= len(self.HashList):
+                index = 0
+            if self.hashList[index] == item:
+                return True
+            if self.HashList[index] == None:
+                return False
+            if index == first:
+                return False
+
+    def items(self):
+        output = []
+        for i in range(self.length - 1):
+            if i != None:
+                output.append(self.HashList[i])
+            else:
+                continue
+        return output
